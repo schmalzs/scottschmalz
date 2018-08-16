24631caf23596b5b6023299a9a821cde4ec2e104
Scott Schmalz <sschmalz@hubspot.com>
2018-08-16 18:34:16 -0400

    hello world!

---
+++ src/server/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+app.listen(PORT, () => console.info(`Server started on port ${PORT}`));
