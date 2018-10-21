21e0604a329f4beb5226a88aa9c56cbab5c5bc34
Scott Schmalz <scott.schmalz@gmail.com>
2018-10-21 15:44:26 -0400

    add favicon

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { DefinePlugin } = require('webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.common.js');
 
@@ -21,6 +22,7 @@ module.exports = merge(common, {
       'process.env': {
         NODE_ENV: JSON.stringify('production')
       }
-    })
+    }),
+    new FaviconsWebpackPlugin('./src/client/static/images/icon.png')
   ]
 });
